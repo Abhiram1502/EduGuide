81a84b888973e0d3d67548a62b78aa81851c7e30
Abhiram Pinnaka <148970953+Abhiram1502@users.noreply.github.com>
2024-12-01 15:09:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let links=document.getElementById("links");
 function showMenu(){
+    links.style.display="flex"
     links.style.right="0px";
     links.style.transition="1s ease-in-out";
 }
 function hideMenu(){
     links.style.right="-180px";
     links.style.transition="1s ease-in-out";
+    links.style.display="none";
 }
